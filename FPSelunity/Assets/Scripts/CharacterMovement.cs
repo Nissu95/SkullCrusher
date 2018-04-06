a504492fccd26377e8d5b35779847a10a314e6f5
unknown <nicosus95@gmail.com>
2018-04-05 21:45:24 -0300

    +CharacterControler, CameraRotation

---
+++ FPSelunity/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    [SerializeField] private float gravity;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+
+    private Vector3 velocity;
+    private CharacterController cc;
+    private float horizontal;
+    private float vertical;
+
+	void Start () {
+        cc = GetComponent<CharacterController>();
+        velocity = Vector3.zero;
+	}
+	
+	void Update () {
+
+        if (!cc.isGrounded)
+        {
+            velocity.y -= gravity * Time.deltaTime;
+        }
+        else
+        {
+            if (cc.isGrounded && Input.GetKeyDown(KeyCode.Space))
+                velocity.y = jumpSpeed;
+            else
+                velocity.y = 0;
+        }
+
+        vertical = Input.GetAxis("Vertical") * moveSpeed;
+        horizontal = Input.GetAxis("Horizontal") * moveSpeed;
+        velocity.x = 0;
+        velocity.z = 0;
+
+        velocity.x += transform.forward.x * vertical;
+        velocity.z += transform.forward.z * vertical;
+
+        velocity.x += transform.right.x * horizontal;
+        velocity.z += transform.right.z * horizontal;
+
+        cc.Move(velocity * Time.deltaTime);
+	}
+}
