c69cd5c013551bd5bd41bc27a9fd80d70917e540
unknown <pehuen-testa@hotmail.com>
2018-05-24 21:30:44 -0300

    animacion ataque y gitignore

---
+++ Assets/Scripts/EnemyIA.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemyIA : MonoBehaviour
+{
+
+    [SerializeField] private Transform tr_Player;
+    [SerializeField] private float speed;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private float raycastDist;
+    [SerializeField] private LayerMask layers;
+    [SerializeField] private string targetTagName;
+    [SerializeField] private float distanceDamage;
+    [SerializeField] private float timeNextAttack;
+
+    private float attackTimer = 0;
+    private Animator anim;
+    
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update() {
+
+        Vector3 diff = tr_Player.position - transform.position;
+        float dist = diff.magnitude;
+        Vector3 dir = diff.normalized;
+
+        if (attackTimer > 0)
+            attackTimer -= Time.deltaTime;
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit, raycastDist, layers))
+        {
+            if (hit.distance <= distanceDamage && attackTimer <= 0)
+            {
+                anim.SetTrigger("Attack");
+                attackTimer = timeNextAttack;
+            }
+        }
+        
+
+        if (!hit.collider)
+        {
+            anim.SetBool("isWalking", false);
+            return;
+        }
+
+        if (hit.collider.tag == targetTagName)
+        {
+            diff.y = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(diff), 0.1f);
+
+            Vector3 mov = dir * speed * Time.deltaTime;
+            mov = Vector3.ClampMagnitude(mov, dist);
+            transform.position += mov;
+            anim.SetBool("isWalking", true);
+        }
+        else
+            anim.SetBool("isWalking", false);
+
+        Debug.DrawLine(
+            transform.position,
+            transform.position + transform.forward * raycastDist, Color.yellow);
+    }
+}
