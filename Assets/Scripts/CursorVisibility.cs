034853580d3b3399c3261daa1fa5ffc485a249ca
unknown <nicosus95@hotmail.com>
2018-06-01 21:37:47 -0300

    +Update lvl 2

---
+++ Assets/Scripts/CursorVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisibility : MonoBehaviour {
+    
+    [SerializeField] bool visible;
+
+	void Start ()
+    {
+
+        if (visible)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+	}
+}
