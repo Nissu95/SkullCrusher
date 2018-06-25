260b449aee1c772a8265319431bc52518d71520b
DESKTOP-91AFLC0\nicos <nicosus95@gmail.com>
2018-06-25 14:50:53 -0300

    +collider enable/disabled

--- fpselu/Assets/Scripts/EnemyDamage.cs
+++ fpselu/Assets/Scripts/EnemyDamage.cs
@@ -6,14 +6,28 @@ public class EnemyDamage : MonoBehaviour {
 
     [SerializeField] string playerTag;
     [SerializeField] float enemyDamage;
+    [SerializeField] float timeCollider;
+    [SerializeField] BoxCollider swordCollider;
+
+    float timer = 0;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
+            swordCollider.enabled = false;
             var myName = GetComponent<EnemyDT>().data.name;
             var playerElementName = PlayerManager.singleton.GetName();
             other.GetComponent<HealthPlayer>().TakeDamage(enemyDamage * BattleManager.singleton.ElementMultiplier(myName, playerElementName));
+
+            if (timer <= 0)
+                timer = timeCollider;
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                swordCollider.enabled = true;
+            }
+
         }
     }
 }
